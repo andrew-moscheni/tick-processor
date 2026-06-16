3aaf812b318807af39b6cd1b5e6fc865f093e121
Andrew Moscheni <agmoscheni@gmail.com>
2026-06-15 22:51:23 -0400

    Wrote simple Python script to fetch historical data in yfinance. Next step is to have the C++ script collect that data from an input it sends for collection.

---
+++ python/stock_data.py
@@ -0,0 +1,24 @@
+import argparse
+import sys
+import yfinance as yf
+import datetime
+import csv
+
+if __name__ == "__main__":
+	# Argument parsing
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-t', '--tickers', nargs='+', required=True, help='List of tickers to get historical data from.')
+	parser.add_argument('-sd', '--start', nargs='+', required=True, help='Start date of ticker tracking.')
+	parser.add_argument('-ed', '--end', nargs='+', required=False, help='End date of ticker tracking.')
+	args=parser.parse_args()
+
+	ticker_list = args.tickers
+	start_date = args.start[0]
+	end_date = args.end if args.end else datetime.date.today().isoformat()
+
+	data = yf.download(ticker_list, start=start_date, end=end_date, group_by='tickers')
+
+	# Using a CSV writer to write to stdout so the parent C++ script can obtain a CSV
+	writer = csv.writer(sys.stdout)
+	writer.writerows(data)
+	sys.stdout.flush()
